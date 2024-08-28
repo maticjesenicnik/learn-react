0783764e70da69016b3da53162718994cc46ed0c
maticjesenicnik <matic.jesenicnik@gmail.com>
2024-08-28 23:13:06 +0200

    refactor and split components to seperate files

---
+++ src/components/Header.jsx
@@ -0,0 +1,19 @@
+import reactEssentialsImage from "../assets/react-core-concepts.png";
+
+const reactDescriptions = ["Fundamental", "Crucial", "Core"];
+
+function getRandomInt(max) {
+  return Math.floor(Math.random() * (max + 1));
+}
+
+export default function Header() {
+  const description = reactDescriptions[getRandomInt(reactDescriptions.length - 1)];
+
+  return (
+    <header>
+      <img src={reactEssentialsImage} alt="Stylized atom" />
+      <h1>React Essentials</h1>
+      <p>{description} React concepts you will need for almost any app you are going to build!</p>
+    </header>
+  );
+}
