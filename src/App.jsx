151bf66e0d2b9d1cd2690b6066b4dd0406ca863b
maticjesenicnik <matic.jesenicnik@gmail.com>
2024-08-28 22:08:17 +0200

    Changed image loading to import

--- src/App.jsx
+++ src/App.jsx
@@ -1,3 +1,5 @@
+import reactEssentialsImage from "./assets/react-core-concepts.png";
+
 const reactDescriptions = ["Fundamental", "Crucial", "Core"];
 
 function getRandomInt(max) {
@@ -9,7 +11,7 @@ function Header() {
 
   return (
     <header>
-      <img src="src/assets/react-core-concepts.png" alt="Stylized atom" />
+      <img src={reactEssentialsImage} alt="Stylized atom" />
       <h1>React Essentials</h1>
       <p>{description} React concepts you will need for almost any app you are going to build!</p>
     </header>
