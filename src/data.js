036a1cf6ccd649d92b8ae90441d47c9728609f81
maticjesenicnik <matic.jesenicnik@gmail.com>
2024-09-02 00:01:58 +0200

    format data.js file

--- src/data.js
+++ src/data.js
@@ -29,8 +29,7 @@ export const CORE_CONCEPTS = [
 export const EXAMPLES = {
   components: {
     title: "Components",
-    description:
-      "Components are the building blocks of React applications. A component is a self-contained module (HTML + optional CSS + JS) that renders some output.",
+    description: "Components are the building blocks of React applications. A component is a self-contained module (HTML + optional CSS + JS) that renders some output.",
     code: `
 function Welcome() {
   return <h1>Hello, World!</h1>;
@@ -38,8 +37,7 @@ function Welcome() {
   },
   jsx: {
     title: "JSX",
-    description:
-      "JSX is a syntax extension to JavaScript. It is similar to a template language, but it has full power of JavaScript (e.g., it may output dynamic content).",
+    description: "JSX is a syntax extension to JavaScript. It is similar to a template language, but it has full power of JavaScript (e.g., it may output dynamic content).",
     code: `
 <div>
   <h1>Welcome {userName}</h1>
@@ -56,8 +54,7 @@ function Welcome(props) {
   },
   state: {
     title: "State",
-    description:
-      "State allows React components to change their output over time in response to user actions, network responses, and anything else.",
+    description: "State allows React components to change their output over time in response to user actions, network responses, and anything else.",
     code: `
 function Counter() {
   const [isVisible, setIsVisible] = useState(false);
