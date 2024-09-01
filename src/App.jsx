5215049e6ba686bc649e747a310e0bc2d41a70db
maticjesenicnik <matic.jesenicnik@gmail.com>
2024-09-01 23:48:35 +0200

    refactored app, split app components into smaller components

--- src/App.jsx
+++ src/App.jsx
@@ -1,77 +1,14 @@
-import { useState } from "react";
-import CoreConcept from "./components/CoreConcept/CoreConcept";
 import Header from "./components/Header/Header";
-import TabButton from "./components/TabButton/TabButton";
-import { CORE_CONCEPTS } from "./data";
-import { EXAMPLES } from "./data";
+import CoreConcepts from "./components/CoreConcepts/CoreConcepts";
+import Examples from "./components/Examples/Examples";
 
 function App() {
-  const [selectedTopic, setSelectedTopic] = useState();
-
-  const handleSelect = (selectedTopic) => {
-    setSelectedTopic(selectedTopic);
-  };
-
   return (
     <>
       <Header />
       <main>
-        <section id="core-concepts">
-          <h2>Core Concepts</h2>
-          <ul>
-            {CORE_CONCEPTS.map((concept, index) => (
-              <CoreConcept key={index} {...concept} />
-            ))}
-          </ul>
-        </section>
-        <section id="examples">
-          <h2>Examples</h2>
-          <menu>
-            <TabButton
-              isSelected={selectedTopic === "components"}
-              onSelect={() => {
-                handleSelect("components");
-              }}
-            >
-              Components
-            </TabButton>
-            <TabButton
-              isSelected={selectedTopic === "jsx"}
-              onSelect={() => {
-                handleSelect("jsx");
-              }}
-            >
-              JSX
-            </TabButton>
-            <TabButton
-              isSelected={selectedTopic === "props"}
-              onSelect={() => {
-                handleSelect("props");
-              }}
-            >
-              Props
-            </TabButton>
-            <TabButton
-              isSelected={selectedTopic === "state"}
-              onSelect={() => {
-                handleSelect("state");
-              }}
-            >
-              State
-            </TabButton>
-          </menu>
-          {!selectedTopic && <p>Please select a topic.</p>}
-          {selectedTopic && (
-            <div id="tab-content">
-              <p></p>
-              <h3>{EXAMPLES[selectedTopic].title}</h3>
-              <p>{EXAMPLES[selectedTopic].description}</p>
-              <pre>
-                <code>{EXAMPLES[selectedTopic].code}</code>
-              </pre>
-            </div>
-          )}
-        </section>
+        <CoreConcepts />
+        <Examples />
       </main>
     </>
   );
---
+++ src/components/CoreConcepts/CoreConcepts.jsx
@@ -0,0 +1,15 @@
+import CoreConcept from "./CoreConcept/CoreConcept";
+import { CORE_CONCEPTS } from "../../data";
+
+export default function CoreConcepts() {
+  return (
+    <section id="core-concepts">
+      <h2>Core Concepts</h2>
+      <ul>
+        {CORE_CONCEPTS.map((concept, index) => (
+          <CoreConcept key={index} {...concept} />
+        ))}
+      </ul>
+    </section>
+  );
+}
