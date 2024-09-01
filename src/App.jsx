01728907207dfddc9ce60325e3c24cdff266c2a7
maticjesenicnik <matic.jesenicnik@gmail.com>
2024-09-01 21:04:45 +0200

    made content display conditionally and add prettier config

--- src/App.jsx
+++ src/App.jsx
@@ -6,7 +6,7 @@ import { CORE_CONCEPTS } from "./data";
 import { EXAMPLES } from "./data";
 
 function App() {
-  const [selectedTopic, setSelectedTopic] = useState("components");
+  const [selectedTopic, setSelectedTopic] = useState();
 
   const handleSelect = (selectedTopic) => {
     setSelectedTopic(selectedTopic);
@@ -56,13 +56,17 @@ function App() {
               State
             </TabButton>
           </menu>
-          <div id="tab-content">
-            <h3>{EXAMPLES[selectedTopic].title}</h3>
-            <p>{EXAMPLES[selectedTopic].description}</p>
-            <pre>
-              <code>{EXAMPLES[selectedTopic].code}</code>
-            </pre>
-          </div>
+          {!selectedTopic && <p>Please select a topic.</p>}
+          {selectedTopic && (
+            <div id="tab-content">
+              <p></p>
+              <h3>{EXAMPLES[selectedTopic].title}</h3>
+              <p>{EXAMPLES[selectedTopic].description}</p>
+              <pre>
+                <code>{EXAMPLES[selectedTopic].code}</code>
+              </pre>
+            </div>
+          )}
         </section>
       </main>
     </div>
