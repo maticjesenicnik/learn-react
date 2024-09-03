b03f7bb8ccbadc67c3f15cf8adb8238427789910
maticjesenicnik <matic.jesenicnik@gmail.com>
2024-09-03 23:18:55 +0200

    add ability to add symbols to the board

--- 02-starting-project-tic-tac-toe/src/components/GameBoard.jsx
+++ 02-starting-project-tic-tac-toe/src/components/GameBoard.jsx
@@ -1,3 +1,5 @@
+import { useState } from "react";
+
 const initialGameBoard = [
   [null, null, null],
   [null, null, null],
@@ -5,14 +7,29 @@ const initialGameBoard = [
 ];
 
 export default function GameBoard() {
+  const [gameBoard, setGameBoard] = useState(initialGameBoard);
+  const [playerSymbol, setPlayerSymbol] = useState("X");
+
+  const handlePlayerTurn = (rowIndex, colIndex) => {
+    setGameBoard(previousBoardGame => {
+      const updatedGameBoard = [...previousBoardGame.map(innerArray => [...innerArray])];
+      updatedGameBoard[rowIndex][colIndex] = playerSymbol;
+      return updatedGameBoard;
+    });
+
+    setPlayerSymbol(previousPlayerSymbol => {
+      return previousPlayerSymbol === "X" ? "O" : "X";
+    });
+  };
+
   return (
     <ol id="game-board">
-      {initialGameBoard.map((row, rowIndex) => (
+      {gameBoard.map((row, rowIndex) => (
         <li key={rowIndex}>
           <ol>
             {row.map((playerSymbol, colIndex) => (
               <li key={colIndex}>
-                <button>{playerSymbol}</button>
+                <button onClick={() => handlePlayerTurn(rowIndex, colIndex)}>{playerSymbol}</button>
               </li>
             ))}
           </ol>
