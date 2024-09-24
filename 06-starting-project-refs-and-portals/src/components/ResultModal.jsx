36f609854c1d8bb3d28d191529a3678007c5fd6c
maticjesenicnik <matic.jesenicnik@gmail.com>
2024-09-24 23:37:58 +0200

    use portal to show the dialog in the div.modal in the DOM

--- 06-starting-project-refs-and-portals/src/components/ResultModal.jsx
+++ 06-starting-project-refs-and-portals/src/components/ResultModal.jsx
@@ -1,4 +1,5 @@
 import { forwardRef, useImperativeHandle, useRef } from "react";
+import { createPortal } from "react-dom";
 
 const ResultModal = forwardRef(function ResultModal({ targetTime, timeRemaining, onReset }, ref) {
   const dialog = useRef();
@@ -15,7 +16,7 @@ const ResultModal = forwardRef(function ResultModal({ targetTime, timeRemaining,
     };
   });
 
-  return (
+  return createPortal(
     <dialog ref={dialog} className="result-modal" onClose={onReset}>
       {userLost && <h2>You lost</h2>}
       {!userLost && <h2>Your score: {score}</h2>}
@@ -34,7 +35,8 @@ const ResultModal = forwardRef(function ResultModal({ targetTime, timeRemaining,
       <form method="dialog" onSubmit={onReset}>
         <button>Close</button>
       </form>
-    </dialog>
+    </dialog>,
+    document.getElementById("modal")
   );
 });
 
