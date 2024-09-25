9d425e2c8f8336a998c26c332a932d6d8ada1d1a
maticjesenicnik <matic.jesenicnik@gmail.com>
2024-09-25 22:52:36 +0200

    remove hardcoded project data

--- 07-starting-project-project-management-app/src/App.jsx
+++ 07-starting-project-project-management-app/src/App.jsx
@@ -3,22 +3,9 @@ import Sidebar from "./components/Sidebar";
 import NoProjectSelected from "./components/NoProjectSelected";
 
 function App() {
-  const [projects, setProjects] = useState([
-    {
-      title: "Learning React",
-      description: "Learn React from the group up. \n\n Start with the basics, finish with advanced knowledge",
-      tasks: [],
-    },
-    {
-      title: "Mastering React",
-      description: "Master it while doing applications. \n\n Enhance your knowledge with doing your own projects",
-      tasks: [],
-    },
-  ]);
-
   return (
     <main className="h-screen mt-8 flex gap-8">
-      <Sidebar projects={projects} />
+      <Sidebar />
       <NoProjectSelected />
     </main>
   );
--- 07-starting-project-project-management-app/src/components/Sidebar.jsx
+++ 07-starting-project-project-management-app/src/components/Sidebar.jsx
@@ -1,20 +1,11 @@
 import Button from "./Button";
 
-export default function Sidebar({ projects }) {
+export default function Sidebar() {
   return (
     <aside className="w-1/3 px-8 py-16 bg-stone-900 text-stone-50 md:w-72 rounded-r-xl">
       <h2 className="mb-8 font-bold uppercase md:text-xl text-stone-200">Your projects</h2>
       <Button>+ Add Project</Button>
-      <ul className="mt-8">
-        {projects.length > 0 &&
-          projects.map((project, index) => (
-            <li key={index}>
-              <button className="w-full text-left px-2 py-1 rounded-sm my-1 hover:text-stone-200 hover:bg-stone-800">
-                {project.title} {index}
-              </button>
-            </li>
-          ))}
-      </ul>
+      <ul className="mt-8"></ul>
     </aside>
   );
 }
