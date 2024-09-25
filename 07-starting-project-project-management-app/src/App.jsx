0e3b744a23daab5c3150b4cc07d51b6638f8724d
maticjesenicnik <matic.jesenicnik@gmail.com>
2024-09-25 22:32:15 +0200

    moved sidebar to seperate component

--- 07-starting-project-project-management-app/src/App.jsx
+++ 07-starting-project-project-management-app/src/App.jsx
@@ -1,4 +1,5 @@
 import { useState } from "react";
+import Sidebar from "./components/Sidebar";
 
 function App() {
   const [projects, setProjects] = useState([
@@ -15,12 +16,8 @@ function App() {
   ]);
 
   return (
-    <main className="h-screen my-8 flex gap-8">
-      <aside className="w-1/3 px-8 py-16 bg-stone-900 text-stone-50 md:w-72 rounded-r-xl">
-        <h2 className="mb-8 font-bold uppercase md:text-xl text-stone-200">Your projects</h2>
-        <button className="px-4 py-2 text-xs md:text-base rounded-md bg-stone-700 text-stone-400 hover:bg-stone-600 hover:text-stone-100">Add Project</button>
-        <ul className="mt-8">{projects.length > 0 && projects.map((project, index) => <li key={index}>{project.title}</li>)}</ul>
-      </aside>
+    <main className="h-screen mt-8 flex gap-8">
+      <Sidebar projects={projects} />
     </main>
   );
 }
