fe0b8888161a64c97a15d3c984ef1bfffbe54294
maticjesenicnik <matic.jesenicnik@gmail.com>
2024-09-25 22:59:28 +0200

    when button for create project is pressed show proper content

--- 07-starting-project-project-management-app/src/App.jsx
+++ 07-starting-project-project-management-app/src/App.jsx
@@ -1,12 +1,35 @@
 import { useState } from "react";
 import Sidebar from "./components/Sidebar";
+import NewProject from "./components/NewProject";
 import NoProjectSelected from "./components/NoProjectSelected";
 
 function App() {
+  const [projectsState, setProjectsState] = useState({
+    selectedProjectId: undefined,
+    projects: [],
+  });
+
+  const handleStartAddProject = () => {
+    setProjectsState(prevState => {
+      return {
+        ...prevState,
+        selectedProjectId: null,
+      };
+    });
+  };
+
+  let content;
+
+  if (projectsState.selectedProjectId === null) {
+    content = <NewProject />;
+  } else if (projectsState.selectedProjectId === undefined) {
+    content = <NoProjectSelected onStartAddProject={handleStartAddProject} />;
+  }
+
   return (
     <main className="h-screen mt-8 flex gap-8">
-      <Sidebar />
-      <NoProjectSelected />
+      <Sidebar onStartAddProject={handleStartAddProject} />
+      {content}
     </main>
   );
 }
