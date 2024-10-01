ebeb3654456e9e2318f4b0d4dbcd9f2fe25f3933
maticjesenicnik <matic.jesenicnik@gmail.com>
2024-10-01 21:11:56 +0200

    add quiz complete logic

--- 10-starting-project-quiz-app/src/components/Quiz.jsx
+++ 10-starting-project-quiz-app/src/components/Quiz.jsx
@@ -1,10 +1,12 @@
 import { useState } from "react";
+import QuizCompleteImage from "../assets/quiz-complete.png";
 import QUESTIONS from "../questions";
 
 export default function Quiz() {
   const [userAnswers, setUserAnswers] = useState([]);
   const activeQuestionIndex = userAnswers.length;
-  const activeQuestion = QUESTIONS[activeQuestionIndex];
+
+  const isQuizComplete = activeQuestionIndex === QUESTIONS.length;
 
   const handleSelectAnswer = selectedAnswer => {
     setUserAnswers(prevAnswer => {
@@ -12,12 +14,25 @@ export default function Quiz() {
     });
   };
 
+  if (isQuizComplete) {
+    return (
+      <div id="summary">
+        <img src={QuizCompleteImage} alt="A trophy showcasing the quiz is complete" />
+        <h2>Quiz completed!</h2>
+      </div>
+    );
+  }
+
+  const activeQuestion = QUESTIONS[activeQuestionIndex];
+  const shuffledAnswers = [...activeQuestion.answers];
+  shuffledAnswers.sort(() => Math.random() - 0.5);
+
   return (
     <div id="quiz">
       <div id="question">
         <h2>{activeQuestion.text}</h2>
         <ul id="answers">
-          {activeQuestion.answers.map((answer, index) => (
+          {shuffledAnswers.map((answer, index) => (
             <li key={index} className="answer">
               <button onClick={() => handleSelectAnswer(answer)}>{answer}</button>
             </li>
