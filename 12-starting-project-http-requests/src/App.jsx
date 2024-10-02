5ee1f1d315d2fbdbbc564663030bba2d9ac19dfe
maticjesenicnik <matic.jesenicnik@gmail.com>
2024-10-02 23:10:00 +0200

    add logic for updating user places

--- 12-starting-project-http-requests/src/App.jsx
+++ 12-starting-project-http-requests/src/App.jsx
@@ -3,13 +3,16 @@ import { useCallback, useRef, useState } from "react";
 import logoImg from "./assets/logo.png";
 import AvailablePlaces from "./components/AvailablePlaces.jsx";
 import DeleteConfirmation from "./components/DeleteConfirmation.jsx";
+import Error from "./components/Error.jsx";
 import Modal from "./components/Modal.jsx";
 import Places from "./components/Places.jsx";
+import { updateUserPlaces } from "./http.js";
 
 function App() {
   const selectedPlace = useRef();
 
   const [userPlaces, setUserPlaces] = useState([]);
+  const [errorUpdatingPlaces, setErrorUpdatingPlaces] = useState(null);
 
   const [modalIsOpen, setModalIsOpen] = useState(false);
 
@@ -22,7 +25,7 @@ function App() {
     setModalIsOpen(false);
   }
 
-  function handleSelectPlace(selectedPlace) {
+  async function handleSelectPlace(selectedPlace) {
     setUserPlaces(prevPickedPlaces => {
       if (!prevPickedPlaces) {
         prevPickedPlaces = [];
@@ -32,6 +35,13 @@ function App() {
       }
       return [selectedPlace, ...prevPickedPlaces];
     });
+
+    try {
+      await updateUserPlaces([selectedPlace, ...userPlaces]);
+    } catch (error) {
+      setUserPlaces(userPlaces);
+      setErrorUpdatingPlaces({ message: error.message || "Failed to update user places " });
+    }
   }
 
   const handleRemovePlace = useCallback(async function handleRemovePlace() {
@@ -40,8 +50,16 @@ function App() {
     setModalIsOpen(false);
   }, []);
 
+  const handleError = () => {
+    setErrorUpdatingPlaces(null);
+  };
+
   return (
     <>
+      <Modal open={errorUpdatingPlaces} onClose={handleError}>
+        {errorUpdatingPlaces && <Error title="An error has occured" message={errorUpdatingPlaces.message} onConfirm={handleError} />}
+      </Modal>
+
       <Modal open={modalIsOpen} onClose={handleStopRemovePlace}>
         <DeleteConfirmation onCancel={handleStopRemovePlace} onConfirm={handleRemovePlace} />
       </Modal>
--- 12-starting-project-http-requests/src/http.js
+++ 12-starting-project-http-requests/src/http.js
@@ -8,3 +8,21 @@ export async function fetchAvailablePlaces() {
 
   return data.places;
 }
+
+export async function updateUserPlaces(places) {
+  const response = await fetch("http://localhost:3000/user-places", {
+    method: "put",
+    body: JSON.stringify({ places }),
+    headers: {
+      "Content-Type": "application/json",
+    },
+  });
+
+  const data = await response.json();
+
+  if (!response.ok) {
+    throw new Error("Failed to update user data.");
+  }
+
+  return data;
+}
