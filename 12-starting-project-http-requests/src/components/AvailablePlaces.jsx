4601cbed08be757052d1da6a64a5a69023932dbc
maticjesenicnik <matic.jesenicnik@gmail.com>
2024-10-02 22:28:01 +0200

    now getting data from the backend asynchronously

--- 12-starting-project-http-requests/src/components/AvailablePlaces.jsx
+++ 12-starting-project-http-requests/src/components/AvailablePlaces.jsx
@@ -5,13 +5,13 @@ export default function AvailablePlaces({ onSelectPlace }) {
   const [availablePlaces, setAvailablePlaces] = useState([]);
 
   useEffect(() => {
-    fetch("http://localhost:3000/places")
-      .then(response => {
-        return response.json();
-      })
-      .then(data => {
-        setAvailablePlaces(data.places);
-      });
+    async function fetchPlaces() {
+      const response = await fetch("http://localhost:3000/places");
+      const data = await response.json();
+      setAvailablePlaces(data.places);
+    }
+
+    fetchPlaces();
   }, []);
 
   return <Places title="Available Places" places={availablePlaces} fallbackText="No places available." onSelectPlace={onSelectPlace} />;
