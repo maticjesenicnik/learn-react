a129bf0d8a6f795d0832d3b7401fcebd71854f6a
maticjesenicnik <matic.jesenicnik@gmail.com>
2024-10-02 21:57:54 +0200

    Initial commit for 12-starting-project-http-requests

---
+++ 12-starting-project-http-requests/src/components/AvailablePlaces.jsx
@@ -0,0 +1,5 @@
+import Places from "./Places.jsx";
+
+export default function AvailablePlaces({ onSelectPlace }) {
+  return <Places title="Available Places" places={[]} fallbackText="No places available." onSelectPlace={onSelectPlace} />;
+}
