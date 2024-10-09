b26504cb80bccdbfbab3c57aaf1e690edc62cd99
maticjesenicnik <matic.jesenicnik@gmail.com>
2024-10-09 20:35:44 +0200

    create custom hook and use it for calling user places

---
+++ 13-starting-project-custom-hooks/src/hooks/useFetch.js
@@ -0,0 +1,25 @@
+import { useEffect, useState } from "react";
+
+export default function useFetch(fetchFunction, initialValue) {
+  const [isFetching, setIsFetching] = useState();
+  const [error, setError] = useState();
+  const [data, setData] = useState(initialValue);
+
+  useEffect(() => {
+    async function fetchData() {
+      setIsFetching(true);
+      try {
+        const fetchedData = await fetchFunction();
+        setData(fetchedData);
+      } catch (error) {
+        setError({ message: error.message || "Failed to fetch data." });
+      }
+
+      setIsFetching(false);
+    }
+
+    fetchData();
+  }, [fetchFunction]);
+
+  return { data, isFetching, error };
+}
