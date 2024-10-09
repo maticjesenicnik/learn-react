126e4b37039bc451fe140b5088d46b88180c5131
maticjesenicnik <matic.jesenicnik@gmail.com>
2024-10-09 22:30:51 +0200

    add validation on focus change

--- 14-starting-project-forms-and-user-input/src/components/Login.jsx
+++ 14-starting-project-forms-and-user-input/src/components/Login.jsx
@@ -5,8 +5,12 @@ export default function Login() {
     email: "",
     password: "",
   });
+  const [isEdited, setIsEdited] = useState({
+    email: false,
+    password: false,
+  });
 
-  const emailIsInvalid = inputData.email !== "" && !inputData.email.includes("@");
+  const emailIsInvalid = isEdited.email && !inputData.email.includes("@");
 
   const handleSubmit = event => {
     event.preventDefault();
@@ -17,6 +21,18 @@ export default function Login() {
       ...prevData,
       [identifier]: value,
     }));
+
+    setIsEdited(prevData => ({
+      ...prevData,
+      [identifier]: false,
+    }));
+  };
+
+  const handleInputBlur = identifier => {
+    setIsEdited(prevData => ({
+      ...prevData,
+      [identifier]: true,
+    }));
   };
 
   return (
@@ -26,13 +42,27 @@ export default function Login() {
       <div className="control-row">
         <div className="control no-margin">
           <label htmlFor="email">Email</label>
-          <input id="email" type="email" name="email" onChange={event => handleInputDataChange("email", event.target.value)} value={inputData.email} />
+          <input
+            id="email"
+            type="email"
+            name="email"
+            onBlur={() => handleInputBlur("email")}
+            onChange={event => handleInputDataChange("email", event.target.value)}
+            value={inputData.email}
+          />
           <div className="control-error">{emailIsInvalid && <p>Please enter a valid email address</p>}</div>
         </div>
 
         <div className="control no-margin">
           <label htmlFor="password">Password</label>
-          <input id="password" type="password" name="password" onChange={event => handleInputDataChange("password", event.target.value)} value={inputData.password} />
+          <input
+            id="password"
+            type="password"
+            name="password"
+            onBlur={() => handleInputBlur("password")}
+            onChange={event => handleInputDataChange("password", event.target.value)}
+            value={inputData.password}
+          />
         </div>
       </div>
 
