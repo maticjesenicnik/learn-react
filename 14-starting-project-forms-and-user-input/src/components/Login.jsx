1bbc58df011c20293da147afd045435970744924
maticjesenicnik <matic.jesenicnik@gmail.com>
2024-10-09 21:50:21 +0200

    add state to manage user forms

--- 14-starting-project-forms-and-user-input/src/components/Login.jsx
+++ 14-starting-project-forms-and-user-input/src/components/Login.jsx
@@ -1,17 +1,35 @@
+import { useState } from "react";
+
 export default function Login() {
+  const [inputData, setInputData] = useState({
+    email: "",
+    password: "",
+  });
+
+  const handleSubmit = event => {
+    event.preventDefault();
+  };
+
+  const handleInputDataChange = (identifier, value) => {
+    setInputData(prevData => ({
+      ...prevData,
+      [identifier]: value,
+    }));
+  };
+
   return (
-    <form>
+    <form onSubmit={handleSubmit}>
       <h2>Login</h2>
 
       <div className="control-row">
         <div className="control no-margin">
           <label htmlFor="email">Email</label>
-          <input id="email" type="email" name="email" />
+          <input id="email" type="email" name="email" onChange={event => handleInputDataChange("email", event.target.value)} value={inputData.email} />
         </div>
 
         <div className="control no-margin">
           <label htmlFor="password">Password</label>
-          <input id="password" type="password" name="password" />
+          <input id="password" type="password" name="password" onChange={event => handleInputDataChange("password", event.target.value)} value={inputData.password} />
         </div>
       </div>
 
