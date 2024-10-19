c104df999bbe625e63d5eb91ce5e31546e398ce5
maticjesenicnik <matic.jesenicnik@gmail.com>
2024-10-19 21:28:16 +0200

    add layout and navigation to the app

--- 18-starting-project-react-router/src/pages/Home.jsx
+++ 18-starting-project-react-router/src/pages/Home.jsx
@@ -1,5 +1,13 @@
 import React from "react";
+import { Link } from "react-router-dom";
 
 export default function Home() {
-  return <h1>My home page!</h1>;
+  return (
+    <>
+      <h1>My home page!</h1>
+      <p>
+        Go to the <Link to="/products">list of products</Link>
+      </p>
+    </>
+  );
 }
