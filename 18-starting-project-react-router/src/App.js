1a42a093a7c9239790d5af3a1ce296775d7efa2b
maticjesenicnik <matic.jesenicnik@gmail.com>
2024-10-19 21:13:05 +0200

    create router and add homepage to it

--- 18-starting-project-react-router/src/App.js
+++ 18-starting-project-react-router/src/App.js
@@ -1,5 +1,10 @@
+import { createBrowserRouter, RouterProvider } from "react-router-dom";
+import HomePage from "./pages/HomePage";
+
+const router = createBrowserRouter([{ path: "/", element: <HomePage /> }]);
+
 function App() {
-  return <div></div>;
+  return <RouterProvider router={router} />;
 }
 
 export default App;
