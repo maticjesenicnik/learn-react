0ea455605b73ec0d84e8a7c49a6740acbd9f7318
maticjesenicnik <matic.jesenicnik@gmail.com>
2024-10-22 22:46:00 +0200

    defering data if the loading is too long

--- 19-starting-project-advanced-react-router/frontend/src/pages/Events.jsx
+++ 19-starting-project-advanced-react-router/frontend/src/pages/Events.jsx
@@ -1,25 +1,32 @@
-import { json, useLoaderData } from "react-router-dom";
+import { Suspense } from "react";
+import { Await, defer, json, useLoaderData } from "react-router-dom";
 import EventsList from "../components/EventsList";
 
 function EventsPage() {
-  const data = useLoaderData();
-  const events = data.events;
+  const { events } = useLoaderData();
 
   return (
-    <>
-      <div style={{ textAlign: "center" }}></div>
-      <EventsList events={events} />
-    </>
+    <Suspense fallback={<p style={{ textAlign: "center" }}>Loading...</p>}>
+      <Await resolve={events}>{loadedEvents => <EventsList events={loadedEvents} />}</Await>
+    </Suspense>
   );
 }
 
 export default EventsPage;
-export async function loader() {
+
+async function loadEvents() {
   const response = await fetch("http://localhost:8080/events");
 
   if (!response.ok) {
     throw json({ message: "Could not fetch events." }, { status: 500 });
   } else {
-    return response;
+    const data = await response.json();
+    return data.events;
   }
+}
+
+export function loader() {
+  return defer({
+    events: loadEvents(),
+  });
 }
