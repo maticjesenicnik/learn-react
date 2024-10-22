76f8457e11a09140c4797c5c040cc513cb134809
maticjesenicnik <matic.jesenicnik@gmail.com>
2024-10-22 20:19:29 +0200

    loading state for when we're waiting for data to be loaded

--- 19-starting-project-advanced-react-router/frontend/src/pages/RootLayout.jsx
+++ 19-starting-project-advanced-react-router/frontend/src/pages/RootLayout.jsx
@@ -2,11 +2,14 @@ import { Outlet } from "react-router-dom";
 import MainNavigation from "../components/MainNavigation";
 
 export default function RootLayout() {
+  // const navigation = useNavigation();
+
   return (
     <>
       <MainNavigation />
       <main>
         <Outlet />
+        {/* {navigation.state === "loading" && <p>Loading...</p>} */}
       </main>
     </>
   );
