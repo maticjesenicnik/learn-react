ab1353d3c5be6c2978b7881f7e15f7bb12dc4142
maticjesenicnik <matic.jesenicnik@gmail.com>
2024-10-22 19:55:47 +0200

    add more events to the events.json file and modify the Events.jsx file with the provided one in the course

--- 19-starting-project-advanced-react-router/frontend/src/pages/Events.jsx
+++ 19-starting-project-advanced-react-router/frontend/src/pages/Events.jsx
@@ -1,18 +1,37 @@
-import React from "react";
-import { Link } from "react-router-dom";
-import { DUMMY_EVENTS } from "../util/events";
+import { useEffect, useState } from "react";
 
-export default function Events() {
+import EventsList from "../components/EventsList";
+
+function EventsPage() {
+  const [isLoading, setIsLoading] = useState(false);
+  const [fetchedEvents, setFetchedEvents] = useState();
+  const [error, setError] = useState();
+
+  useEffect(() => {
+    async function fetchEvents() {
+      setIsLoading(true);
+      const response = await fetch("http://localhost:8080/events");
+
+      if (!response.ok) {
+        setError("Fetching events failed.");
+      } else {
+        const resData = await response.json();
+        setFetchedEvents(resData.events);
+      }
+      setIsLoading(false);
+    }
+
+    fetchEvents();
+  }, []);
   return (
     <>
-      <h1>Events</h1>
-      <ul>
-        {DUMMY_EVENTS.map(event => (
-          <li key={event.id}>
-            <Link to={event.id}>{event.title}</Link>
-          </li>
-        ))}
-      </ul>
+      <div style={{ textAlign: "center" }}>
+        {isLoading && <p>Loading...</p>}
+        {error && <p>{error}</p>}
+      </div>
+      {!isLoading && fetchedEvents && <EventsList events={fetchedEvents} />}
     </>
   );
 }
+
+export default EventsPage;
