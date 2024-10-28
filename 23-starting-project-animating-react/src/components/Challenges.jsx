e36afd2d2c052d8d17192aa3d786131a2b7383df
maticjesenicnik <matic.jesenicnik@gmail.com>
2024-10-28 22:22:30 +0100

    animated the tab indicator and challenges lists

--- 23-starting-project-animating-react/src/components/Challenges.jsx
+++ 23-starting-project-animating-react/src/components/Challenges.jsx
@@ -37,7 +37,16 @@ export default function Challenges() {
       <ChallengeTabs challenges={filteredChallenges} onSelectType={handleSelectType} selectedType={selectedType}>
         <AnimatePresence mode="wait">
           {displayedChallenges.length > 0 && (
-            <motion.ol key="list" exit={{ y: -30, opacity: 0 }} className="challenge-items">
+            <motion.ol
+              key="list"
+              initial={{
+                opacity: 0,
+                y: -20,
+              }}
+              animate={{ opacity: 1, y: 0 }}
+              exit={{ y: -30, opacity: 0 }}
+              className="challenge-items"
+            >
               <AnimatePresence>
                 {displayedChallenges.map(challenge => (
                   <ChallengeItem key={challenge.id} challenge={challenge} onViewDetails={() => handleViewDetails(challenge.id)} isExpanded={expanded === challenge.id} />
