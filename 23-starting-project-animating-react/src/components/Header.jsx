b74b3783d0237b49822f2d01a0ebb5f6dbba5339
maticjesenicnik <matic.jesenicnik@gmail.com>
2024-10-28 21:34:44 +0100

    animating button hover

--- 23-starting-project-animating-react/src/components/Header.jsx
+++ 23-starting-project-animating-react/src/components/Header.jsx
@@ -1,4 +1,4 @@
-import { AnimatePresence } from "framer-motion";
+import { AnimatePresence, motion } from "framer-motion";
 import { useState } from "react";
 
 import NewChallenge from "./NewChallenge.jsx";
@@ -20,9 +20,9 @@ export default function Header() {
 
       <header id="main-header">
         <h1>Your Challenges</h1>
-        <button onClick={handleStartAddNewChallenge} className="button">
+        <motion.button whileHover={{ scale: 1.1 }} transition={{ type: "spring", stiffness: 500 }} onClick={handleStartAddNewChallenge} className="button">
           Add Challenge
-        </button>
+        </motion.button>
       </header>
     </>
   );
