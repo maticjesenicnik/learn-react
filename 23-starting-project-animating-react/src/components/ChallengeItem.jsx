02bec08fc79feb6c3d1c13c4a73892a90c997c18
maticjesenicnik <matic.jesenicnik@gmail.com>
2024-10-28 22:16:54 +0100

    combining animations with layout animations

--- 23-starting-project-animating-react/src/components/ChallengeItem.jsx
+++ 23-starting-project-animating-react/src/components/ChallengeItem.jsx
@@ -1,4 +1,4 @@
-import { motion } from "framer-motion";
+import { AnimatePresence, motion } from "framer-motion";
 import { useContext } from "react";
 
 import { ChallengesContext } from "../store/challenges-context.jsx";
@@ -46,11 +46,13 @@ export default function ChallengeItem({ challenge, onViewDetails, isExpanded }) 
             </button>
           </p>
 
-          {isExpanded && (
-            <div>
-              <p className="challenge-item-description">{challenge.description}</p>
-            </div>
-          )}
+          <AnimatePresence>
+            {isExpanded && (
+              <motion.div initial={{ height: 0, opacity: 0 }} animate={{ height: "auto", opacity: 1 }} exit={{ height: 0, opacity: 0 }}>
+                <p className="challenge-item-description">{challenge.description}</p>
+              </motion.div>
+            )}
+          </AnimatePresence>
         </div>
       </article>
     </motion.li>
