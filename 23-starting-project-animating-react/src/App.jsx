211b78f4cf0ac2e4668473253705e0bfe484c65e
maticjesenicnik <matic.jesenicnik@gmail.com>
2024-10-28 20:44:33 +0100

    Initial commit for 23-starting-project-animating-react

---
+++ 23-starting-project-animating-react/src/App.jsx
@@ -0,0 +1,15 @@
+import { RouterProvider, createBrowserRouter } from "react-router-dom";
+
+import ChallengesPage from "./pages/Challenges.jsx";
+import WelcomePage from "./pages/Welcome.jsx";
+
+const router = createBrowserRouter([
+  { path: "/", element: <WelcomePage /> },
+  { path: "/challenges", element: <ChallengesPage /> },
+]);
+
+function App() {
+  return <RouterProvider router={router} />;
+}
+
+export default App;
