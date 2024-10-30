f525294f8bd3cafc718aecb2bf6171c97ac310f5
maticjesenicnik <matic.jesenicnik@gmail.com>
2024-10-30 23:54:49 +0100

    add test to test suite

--- 27-starting-project-unit-tests/src/components/Greeting.test.jsx
+++ 27-starting-project-unit-tests/src/components/Greeting.test.jsx
@@ -1,9 +1,11 @@
 import { render, screen } from "@testing-library/react";
 import Greeeting from "./Greeeting";
 
-test("renders Hello World as a text", () => {
-  render(<Greeeting />);
+describe("Greeting component", () => {
+  test("renders Hello World as a text", () => {
+    render(<Greeeting />);
 
-  const helloWorldElement = screen.getByText("Hello World", { exact: false });
-  expect(helloWorldElement).toBeInTheDocument();
+    const helloWorldElement = screen.getByText("Hello World", { exact: false });
+    expect(helloWorldElement).toBeInTheDocument();
+  });
 });
